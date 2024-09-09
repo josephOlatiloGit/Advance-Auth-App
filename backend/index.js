9ea7f1e598f51706d93912e7223b8b5fa36484d1
tobiolatiloGit24 <olatilotobi@gmail.com>
2024-09-09 16:41:39 +0100

    creating auth route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { connectDB } from "../db/connectDB.js";
 import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 dotenv.config(); // To access the env file
@@ -8,6 +9,9 @@ dotenv.config(); // To access the env file
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
+
+// middlewares
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
   connectDB();
