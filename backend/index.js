80b01214ed04f14bfa58b42ea70911968ffcdd11
tobiolatiloGit24 <olatilotobi@gmail.com>
2024-09-09 16:11:57 +0100

    connect the Db

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 import express from "express";
+import { connectDB } from "../db/connectDB.js";
+import dotenv from "dotenv";
 
 const app = express();
+dotenv.config(); // To access the env file
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
 app.listen(3000, () => {
+  connectDB();
   console.log("Server running on port 3000");
 });
