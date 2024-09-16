4639f10ddaa8783f5b7cdb607c2bcbeafe4bbdfe
tobiolatiloGit24 <olatilotobi@gmail.com>
2024-09-16 11:14:38 +0100

    add a logout button to the dashboard

--- frontend/src/pages/DashboardPage.jsx
+++ frontend/src/pages/DashboardPage.jsx
@@ -5,6 +5,9 @@ import { formatDate } from "../utils/date";
 
 export default function DashboardPage() {
   const { user } = useAuthStore();
+
+  const handleLogout = async () => {};
+
   return (
     <motion.div
       initial={{ opacity: 0, scale: 0.9 }}
@@ -54,6 +57,24 @@ export default function DashboardPage() {
           </p>
         </motion.div>
       </div>
+
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.6 }}
+        className="mt-4"
+      >
+        <motion.button
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+          onClick={handleLogout}
+          className="w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
+      font-bold rounded-lg shadow-lg hover:from-green-600 hover:to-emerald-700
+       focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 focus:ring-offset-gray-900"
+        >
+          Logout
+        </motion.button>
+      </motion.div>
     </motion.div>
   );
 }
