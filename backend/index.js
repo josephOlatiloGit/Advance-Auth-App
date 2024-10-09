5915fa05e993db3333c8d3cf256478b3b3ce9013
tobiolatiloGit24 <olatilotobi@gmail.com>
2024-10-09 20:13:50 +0100

    run start script for production for both the server side and client side

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cookieParser from "cookie-parser";
+import path from "path";
 import { connectDB } from "../db/connectDB.js";
 import dotenv from "dotenv";
 import cors from "cors";
@@ -8,11 +9,22 @@ import authRoutes from "./routes/auth.route.js";
 const app = express();
 dotenv.config(); // To access the env file
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 // middlewares
 app.use(express.json()); // allows us to parse incoming request
 app.use(cookieParser()); //allows us to parse incoming cookies
 app.use("/api/auth", authRoutes);
+
+// Make the frontend folder the static asset:
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
